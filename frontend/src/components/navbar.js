d3fe788342592b28e18d6a22fa9be77c3ae35290
dexterwwhite <dexterwwhite@tamu.edu>
2023-04-05 10:08:35 -0500

    Navbar design updates and logout functionality added

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -1,8 +1,34 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 import "../style/navbar.css";
 
 class NavBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            userId: props.userId
+        };
+    }
+
+    logout = async () => {
+        this.props.updateFunc("-1");
+        const request = {
+            method: "DELETE",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            credentials: 'include'
+        }
+        const response = await fetch(`http://localhost:8000/Account/Logout`, request);
+        if(response.status === 200) {
+            this.props.updateFunc("-1");
+            this.setState({ userId: "-1" });
+        } else {
+            console.log("Logout failed.");
+        }
+    }
+
     render() {
         if(this.props.userId === "-1") {
             return (
@@ -12,13 +38,16 @@ class NavBar extends Component {
                     </h1>
                     <ul className="Nav-Button-List">
                         <li>
+                            <Link className="Nav-Button" to="/">Search</Link>
+                        </li>
+                        <li>
                             <Link className="Nav-Button" to="/about">About</Link>
                         </li>
                         <li>
                             <Link className="Nav-Button" to="/login">Login</Link>
                         </li>
                         <li>
-                            <Link className="Nav-Button" to="/signup">Sign-up</Link>
+                            <Link className="Nav-Button" to="/signup">Signup</Link>
                         </li>
                     </ul>
                 </nav>
@@ -31,10 +60,16 @@ class NavBar extends Component {
                     </h1>
                     <ul className="Nav-Button-List">
                         <li>
+                            <Link className="Nav-Button" to="/">Search</Link>
+                        </li>
+                        <li>
                             <Link className="Nav-Button" to="/about">About</Link>
                         </li>
                         <li>
                             <Link className="Nav-Button" to="/profile">Profile</Link>
+                        </li>
+                        <li>
+                            <button className="Nav-Button" onClick={this.logout}>Logout</button>
                         </li>
                     </ul>
                 </nav>
