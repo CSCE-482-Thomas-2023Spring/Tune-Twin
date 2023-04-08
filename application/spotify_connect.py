8a96c193b77d2944e7218e405c5d9a5aa3662c73
Toadapple1 <71990502+Toadapple1@users.noreply.github.com>
2023-04-08 13:42:15 -0500

    Spotify Oauth no database access yet

--- application/spotify_connect.py
+++ application/spotify_connect.py
@@ -1,26 +1,32 @@
 from __main__ import app
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, url_for
 import spotipy
 import configparser
 from spotipy.oauth2 import SpotifyOAuth
-
 config = configparser.ConfigParser()
 config.read('spotify.cfg')
-sp_oauth = SpotifyOAuth(
-    CLIENT_ID = config.get('Spotify', 'CLIENT_ID'),
-    CLIENT_SECRET = config.get('Spotify', 'CLIENT_SECRET'),
-    redirect_uri='http://localhost:5000/callback',
-    scope='user-read-email'
-)
+CLIENT_ID = config.get('Spotify', 'CLIENT_ID')
+CLIENT_SECRET = config.get('Spotify', 'CLIENT_SECRET')
+def create_oauth():
+
+    return SpotifyOAuth(
+        client_id = CLIENT_ID,
+        client_secret = CLIENT_SECRET,
+        redirect_uri=url_for('callback', _external = True),
+        scope='user-read-email'
+    )
+
 @app.route('/callback')
 def callback():
     code = request.args.get('code')
+    sp_oauth = create_oauth()
     token_info = sp_oauth.get_access_token(code)
     access_token = token_info['access_token']
     # Store the access token in a session or database for later use
     return redirect('/')
 
-@app.route('/login')
-def login():
+@app.route('/')
+def loginSpotify():
+    sp_oauth = create_oauth()
     auth_url = sp_oauth.get_authorize_url()
     return redirect(auth_url)
