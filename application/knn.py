f2f3b5f2b85af3e7a89a15707d7fe72fa8893f92
Roman0180 <rgasiorowski01@gmail.com>
2023-04-10 05:02:19 -0500

    fixed blacklist recommendation list

--- application/knn.py
+++ application/knn.py
@@ -19,16 +19,17 @@ client_credentials_manager = SpotifyClientCredentials(
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 
+
 def get_track_features(track_id):
     track_info = sp.track(track_id)
     track_features = sp.audio_features(track_id)
     features = {
         "id": track_id,
         "name": track_info["name"],
-        "album": None,
-        "album_id": None,
+        "album": None, 
+        "album_id":None, 
         "artists": track_info["artists"][0]["name"],
-        "artist_ids": None,
+        "artist_ids": None, 
         "danceability": track_features[0]["danceability"],
         "energy": track_features[0]["energy"],
         "key": track_features[0]["key"],
@@ -44,115 +45,50 @@ def get_track_features(track_id):
     return features
 
 
-def get_nearest_neighbors(track, k, use_cache=False):
+def get_nearest_neighbors(track_id, k, use_cache=False):
     cache_file = "kdtree_cache.pkl"
-    target_features = [
-        "danceability",
-        "energy",
-        "loudness",
-        "mode",
-        "speechiness",
-        "acousticness",
-        "instrumentalness",
-        "liveness",
-        "valence",
-        "tempo",
-        "key",
-    ]
     if use_cache and os.path.isfile(cache_file):
         # Load k-d tree from cache
         with open(cache_file, "rb") as f:
-            print("opening from cache")
+            print('opening from cache')
             tree = pickle.load(f)
         with open("dataframe.pickle", "rb") as f:
             track_df = pickle.load(f)
     else:
-        track_df = pd.read_csv("song_data.csv")[
-            [
-                "0",
-                "1",
-                "2",
-                "3",
-                "4",
-                "5",
-                "6",
-                "7",
-                "8",
-                "9",
-                "10",
-                "11",
-                "12",
-                "13",
-                "14",
-                "15",
-                "id",
-                "name",
-                "album",
-                "album_id",
-                "artists",
-                "artist_ids",
-                "danceability",
-                "energy",
-                "key",
-                "loudness",
-                "mode",
-                "speechiness",
-                "acousticness",
-                "instrumentalness",
-                "liveness",
-                "valence",
-                "tempo",
-            ]
-        ]
+        track_df = pd.read_csv("songdatamerge.csv")
         scaler = MinMaxScaler()
+
+        targetFeatures = [
+            "danceability",
+            "energy",
+            "key",
+            "loudness",
+            "mode",
+            "speechiness",
+            "acousticness",
+            "instrumentalness",
+            "liveness",
+            "valence",
+            "tempo",
+        ]
+
         # All feature data for a track will be between columns 6 - end of the dataframe (this includes things like acousticness, danceability, etc)
-        track_df_scaled = pd.DataFrame(track_df[
-                [
-                    "0",
-                    "1",
-                    "2",
-                    "3",
-                    "4",
-                    "5",
-                    "6",
-                    "7",
-                    "8",
-                    "9",
-                    "10",
-                    "11",
-                    "12",
-                    "13",
-                    "14",
-                    "15"
-                ]
-            ]
-        )
-        track_df_scaled = np.nan_to_num(track_df_scaled, nan=0)
+        track_df_scaled = pd.DataFrame(scaler.fit_transform(track_df[targetFeatures]))
         # Build k-d tree
-        tree = KDTree(track_df_scaled)
+        tree = KDTree(track_df[targetFeatures])
         # Save k-d tree to cache
         with open(cache_file, "wb") as f:
             pickle.dump(tree, f)
         with open("dataframe.pickle", "wb") as f:
             pickle.dump(track_df, f)
     # Find nearest neighbors
-    # simplified_features = []
-    # for feature in target_features:
-    #     simplified_features.append(track_features[feature])
-    # point = np.array([list(simplified_features.values())])
-    point = []
-    for i in range(0, 16):
-        point.append(track[str(i)])
-    # print(len(track_df[0]))
-    print(point)
-    _, indices = tree.query([point], k)
+    point = np.array([list(get_track_features(track_id).values())[6:]])
+    _, indices = tree.query(point, k)
     nearest_neighbors = []
     for i in indices[0]:
         nearest_neighbors.append(track_df.iloc[i])
-    print(nearest_neighbors[0])
     # returns a list of data frame series object
     return nearest_neighbors
-
 
 # input_track_id = "1p80LdxRV74UKvL8gnD7ky"
 # nearest_neighbors = get_nearest_neighbors(input_track_id, k=20)
--- application/music_route.py
+++ application/music_route.py
@@ -13,30 +13,16 @@ from profile_details_route import get_blacklist_details
 from knn import get_nearest_neighbors
 from knn import get_track_features
 from itertools import chain
-from keras.models import load_model
-import tensorflow as tf
-import pandas as pd
-from autoencoder import Autoencoder
-import os
 
 
 @app.route("/Music", methods=["GET"])
 def get_song_recommendations():
     track = request.args.get("query")
     user_email = request.args.get("user_email")
-    features = {
-        "danceability": request.args.get("danceability"),
-        "energy": request.args.get("energy"),
-        "key": request.args.get("key"),
-        "loudness": request.args.get("loudness"),
-        "mode": request.args.get("mode"),
-        "speechiness": request.args.get("speechiness"),
-        "acousticness": request.args.get("acousticness"),
-        "instrumentalness": request.args.get("instrumentalness"),
-        "liveness": request.args.get("liveness"),
-        "valence": request.args.get("valence"),
-        "tempo": request.args.get("tempo"),
-    }
+    print("EMAILLLLL: ", user_email, "TRACK: ", track)
+    features = request.args.get("features")
+    # Get features of input track
+    # input_features = get_features(track)
     # Get song recommendations
     recommendations = get_recommendations(track, features)
     # Filter by blacklist
@@ -47,6 +33,7 @@ def get_song_recommendations():
     target_features = [
         "danceability",
         "energy",
+        "key",
         "loudness",
         "mode",
         "speechiness",
@@ -55,13 +42,12 @@ def get_song_recommendations():
         "liveness",
         "valence",
         "tempo",
-        "key",
     ]
 
     input_features = get_track_features(track)
     # print(input_features, type(input_features), type(filtered_recommendations[0]))
     for recommendation in filtered_recommendations:
-        for index, feature in enumerate(target_features):
+        for feature in target_features:
             recommendation[feature] = gaussian(
                 recommendation.get(feature), input_features.get(feature)
             )
@@ -80,14 +66,22 @@ def filter_recommendations(recommendations, user_email):
         return recommendations
     # query for blacklist
     user_blacklist = get_blacklist_details(user_email)
+    
     # filter songs
     filtered_recommendations = []
-    for song in user_blacklist.get("song_list"):
-        for recommendation in recommendations:
-            if len(song) > 1:
-                # the blacklisted track id will be the first element of song
-                if song[0] != recommendation.get("id"):
-                    filtered_recommendations.append(recommendation)
+    blacklist = []
+    for song in user_blacklist.get('song_list'):
+        if len(song) > 0:
+            blacklist.append(song[0])
+    for recommendation in recommendations:
+        if recommendation.get('id') not in blacklist:
+            filtered_recommendations.append(recommendation)
+        # for recommendation in recommendations:
+        #     print("SONG: ", song[0], " REC:::", recommendation.get("id"))
+        #     if len(song) > 1:
+        #         # the blacklisted track id will be the first element of song
+        #         if song[0] != recommendation.get("id"):
+        #             filtered_recommendations.append(recommendation)
     # filter artists
     for artist in user_blacklist.get("artists"):
         for recommendation in filtered_recommendations:
@@ -102,6 +96,7 @@ def format_data(track, feature_info):
     URL = f"https://api.spotify.com/v1/tracks/{track}"
     data = requests.get(url=URL, headers=header).json()
     track_data = defaultdict(str, data)
+    print(track_data)
     return [
         {
             "album name": track_data.get("album").get("name"),
@@ -109,7 +104,6 @@ def format_data(track, feature_info):
             "track name": track_data.get("name"),
             "track id": track_data.get("id"),
             "artist name": track_data.get("artists")[0].get("name"),
-            "artist id": track_data.get("artists")[0].get("id"),
             "sample": track_data.get("preview_url"),
             "genres": track_data.get("album").get("genres"),
             "track_token": track,
@@ -135,28 +129,10 @@ def gaussian(x, y, sigma=1):
     return math.exp(exponent)
 
 
-def get_recommendations(track, user_features):
-    # get the features of the track
-    track_features = get_track_features(track)
-    # update the feature weights based on the user_features
-    for feature in user_features:
-        if user_features[feature] != None:
-            track_features[feature] = track_features[feature] * user_features[feature]
-    # normalize the numerical feature values of the track
-    target_vals = tf.keras.utils.normalize(list(track_features.values())[6:])
-    # pass features into machine learning model
-    model = Autoencoder()
-    model.built = True
-    model.load_weights("model_weights/.index")
-    preds = model.encoder(target_vals).numpy()
-    # pred = model.predict(np.asarray(train_input_sequences, dtype='int'))
-    # find nearest neighbors based on track_features
-    combined_values = {}
-    for index,value in enumerate(preds[0]):
-        combined_values[str(index)] = value
-    for feature in track_features:
-        combined_values[feature] = track_features[feature]
-    recommendations = get_nearest_neighbors(combined_values, k=15)
+def get_recommendations(track, features):
+    # find nearest neighbors based on track
+    print(track)
+    recommendations = get_nearest_neighbors(track, k=3)
     return recommendations
 
 
